629a3765c1dd2a87e0895dc52937b9c781c150b8
Andressa Helena <handressa125@gmail.com>
2022-09-06 23:00:45 -0300

    Atualização

---
+++ js/title.js
@@ -0,0 +1,15 @@
+
+console.log("Oi");
+const titulo = document.querySelector('.title__bio');
+
+function escrever(elemento){
+    const textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(()=>{
+            elemento.innerHTML += letra;
+        }, 75 * i)
+    });
+}
+
+escrever(titulo);
